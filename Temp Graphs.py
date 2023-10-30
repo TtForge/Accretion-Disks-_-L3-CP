99b7b93e36a7b8dd6f1cf684591499e76b34543c
TtForge <tobyforge@gmail.com>
2023-10-30 15:01:22 +0000

    Temperature plotting working

--- Temp Graphs.py
+++ Temp Graphs.py
@@ -1,3 +1,4 @@
+from cProfile import label
 from distutils.log import Log
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -20,6 +21,8 @@ Acc_rate = 1000000000000000                 # Kg per sec
 R_g = (G * M) / c**2                        # metres
 R_in = 6 * R_g                              # metres
 R_out = 100000 * R_g                        # metres
+r_in = 6
+r_out = 100000
 
 nu_st = math.pow(10, 14)
 nu_end = math.pow(10, 19)
@@ -30,22 +33,62 @@ LogR_out = np.log10(R_out)
 lognu_st = np.log10(nu_st)
 lognu_end = np.log10(nu_end)
 
-def Temp (R):   
-    numerator = G * M * Acc_rate
-    denominator = 8 * np.pi * math.pow(R,3) * SB
-    x = math.pow((numerator/denominator),1/4)
-    return x
+# def Temp (R):   
+#     numerator = G * M * Acc_rate
+#     denominator = 8 * np.pi * math.pow(R,3) * SB
+#     x = math.pow((numerator/denominator),1/4)
+#     return x
 
+def Temp (R):
+    constant = (G * M * Acc_rate) / (8 * np.pi * SB)
+    T4 = constant * 1/(pow(R,3))
+    return pow(T4, 0.25)
+
+def TempViscR (R):
+    constant = (3 * G * M * Acc_rate)/(8 * np.pi * SB)
+    T4 = constant * 1/(pow(R,3)) * (1 - pow((R_in/R), 0.5))
+    return pow(T4, 0.25)
+    
 steps = 1000
-LogRArr = np.linspace(logR_in,LogR_out, steps)
-RArr = np.linspace(R_in,R_out,steps)
+# LogRArr = np.logspace(R_in,R_out, steps, True, 10)
+LogRArr = np.linspace(np.log10(R_in),np.log10(R_out),steps)
 
-LogTempArr = []
-for i in range(0, len(LogRArr)):
-    LogTempArr.append(Temp(LogRArr[i]))
-    
 TempArr = []
-for i in range(0, len(RArr)):
-    TempArr.append(Temp(RArr[i]))
-    
-plt.loglog(RArr,TempArr)
+for i in range(0, len(LogRArr)):
+    TempArr.append(Temp(10**LogRArr[i]))
+
+TempViscArrR = []
+for i in range(0, len(LogRArr)):
+    TempViscArrR.append(TempViscR(10**LogRArr[i]))
+TempViscArrR[0] = 0
+
+plt.plot(LogRArr, TempArr, label = "Temp")
+plt.plot(LogRArr, TempViscArrR, label = "Temp + Visc")
+plt.legend()
+plt.show()
+
+
+# print((3 * G * M * Acc_rate)/(8 * np.pi * SB))
+# print((((3 * G * M * Acc_rate)/(8 * np.pi * SB) * (1/((10**LogRArr[0])**3)) * (1-pow(R_in/(10**LogRArr[0]), 0.5))), 0.25))
+# print(TempArr)
+# print(TempViscArrR)
+
+
+
+
+
+# def Flux (T, nu):
+#     numerator = 2 * np.pi * h * math.pow(nu, 3) / math.pow(c, 2)
+#     denominator = np.exp((h * nu)/(k_B * T) - 1)
+#     x = np.pi * numerator/denominator
+#     return x
+
+# nu = 10000000000000000
+
+# FluxArr = []
+# for i in range(0, len(RArr)):
+#     FluxArr.append(Flux(TempArr[i], nu))
+
+# plt.plot(RArr, FluxArr, label = "Flux")
+# plt.xscale('log')
+# plt.legend()
