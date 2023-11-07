d3ad2775df176c20b1b804b7edebc780c83de5a0
TtForge <tobyforge@gmail.com>
2023-11-07 22:23:54 +0000

    Total luminosity vs steps plotted
    8.89 ish for 1000 bins

--- Temp Graphs.py
+++ Temp Graphs.py
@@ -28,12 +28,22 @@ lognu_end = 19
 
 #####################################################################
 
+def MaxVal(y):
+    i = np.argmax(y)
+    return i+1
+
+#####################################################################
+
 Rsteps = 1000
 nusteps = 1000
 print(Rsteps)
 print(nusteps)
 LogRArr = np.linspace(np.log10(R_in),np.log10(R_out), Rsteps)
 LogNuArr = np.linspace(lognu_st, lognu_end, nusteps)
+def LogRArrVar (steps):
+    return np.linspace(np.log10(R_in),np.log10(R_out), steps)
+def LogNuArrVar (steps):
+    return np.linspace(lognu_st, lognu_end, steps)
 
 #####################################################################
 ### Temperature Functions ###########################################
@@ -51,15 +61,12 @@ def TempViscR (R):
 def TempArrFnc (fnc, R):
     arr = []
     for i in range(0,len(R)):
-        arr.append(fnc(10**R[i]))
+        arr.append(fnc(np.power(10, R[i])))
     return arr
 
 TempArr = TempArrFnc(Temp, LogRArr)
 TempViscArr = TempArrFnc(TempViscR, LogRArr)
 TempViscArr[0] = 0.0000000000000001
-
-print(TempArr)
-print(TempViscArr)
 
 #####################################################################
 ### Temperature Plotting ############################################
@@ -94,16 +101,35 @@ plt.legend()
 plt.show()
 
 #####################################################################
+### Flux vs Temp Plotting ###########################################
+
+for i in range (14, 20):
+    plt.scatter(TempViscArr, np.log10(FluxArray(TempViscArr, pow(10, i))), label = ("v = 10^" + str(i)), marker = 'x', s=10,  linewidth=0.5)
+    # plt.plot(TempViscArr, np.log10(FluxArray(TempViscArr, pow(10, i))), label = ("v = 10^" + str(i)))
+plt.xlabel('Temperature / $K$')
+plt.ylabel('Log(Flux) / $J M^{-1}$')
+plt.legend()
+plt.show()
+
+for i in range (14, 20):
+    plt.scatter(TempViscArr, FluxArray(TempViscArr, pow(10, i)), label = "v = 10^" + str(i), marker = 'x', s=10, linewidth=0.5)
+    # plt.plot(TempViscArr, FluxArray(TempViscArr, pow(10, i)), label = "v = 10^" + str(i))
+plt.xlabel('Temperature / $K$')
+plt.ylabel('Flux / $J M^{-1}$')
+plt.legend()
+plt.show()
+
+#####################################################################
 ### Integration and Luminosity Functions ############################
 
 def integrate (x, y, nu):
     total = 0
     for i in range(0, len(x)-1):
-        h = x[i+1] - x[i]
-        total += h * (y(x[i], nu) + 4*y(x[i]+0.5*h, nu)+y(x[i]+h, nu))
-    return 1/6 * total
+        h = abs(x[i+1] - x[i])
+        total += h/3 * (y(x[i], nu) + 4*y(x[i]+0.5*h, nu) + y(x[i+1], nu))
+    return total
 
-def integrate2 (x, y, nu):
+# def integrate2 (x, y, nu):
     total = 0
     steps = int(len(x) / 2)
     for i in range(0, steps-1):
@@ -114,11 +140,11 @@ def integrate2 (x, y, nu):
 def integrate_Lum (nu, Lum, LogR):
     total = 0
     for i in range(0, len(nu)-1):
-        h = nu[i+1] - nu[i]
-        total += h * (Lum(LogR, nu[i]) + 4* Lum(LogR, nu[i]+0.5*h) + Lum(LogR, nu[i] + h))
-    return 1/6 * total
+        h = abs(nu[i+1] - nu[i])
+        total += h/3 * (Lum(LogR, nu[i]) + 4* Lum(LogR, nu[i]+0.5*h) + Lum(LogR, nu[i] + h))
+    return total
 
-def integrate_Lum2 (nu, Lum, LogR):
+# def integrate_Lum2 (nu, Lum, LogR):
     total = 0
     steps = int(len(nu) / 2)
     for i in range(0, steps-1):
@@ -129,27 +155,51 @@ def integrate_Lum2 (nu, Lum, LogR):
 def integrand (R, nu):
     return Flux(TempViscR(R), nu) * 4 * np.pi * R
 
-def Luminosity (R, nu):
+def Luminosity (LogR, nu):
     Lum = []
     for i in range(0, len(nu)):
-        Lum.append(integrate2(np.power(10, R), integrand, nu[i]) * nu[i])
+        Lum.append(integrate(np.power(10, LogR), integrand, nu[i]) * nu[i])
     return Lum
 
-def LuminosityFnc (R, nu):
-    return integrate2(np.power(10, R), integrand, nu)
+def LuminosityFnc (LogR, nu):
+    return integrate(np.power(10, LogR), integrand, nu)
 
 #####################################################################
 ### Luminosity Total and Plotting ###################################
 
-vLum_vals = Luminosity(LogRArr, np.power(10, LogNuArr))
-plt.plot(LogNuArr, np.log10(vLum_vals))
-plt.xlabel('$Log_{10}($\nu$)$ / $Hz$')
-plt.ylabel('$Log_{10}(L)$ / $W$')
+j = MaxVal(TempViscArr)
+
+# vLum_vals1 = Luminosity(LogRArr[:j], np.power(10, LogNuArr[:j]))
+# plt.plot(LogNuArr[:j], np.log10(vLum_vals1))
+
+# vLum_vals2 = Luminosity(LogRArr[j:], np.power(10, LogNuArr[j:]))
+# plt.plot(LogNuArr[j:], np.log10(vLum_vals2))
+# plt.xlabel('$Log_{10}($\nu$)$ / $Hz$')
+# plt.ylabel('$Log_{10}($\nu$L)$ / $W$')
+# plt.show()
+
+Lum_Tot = integrate_Lum(np.power(10, LogNuArr[j:]), LuminosityFnc, LogRArr[j:]) # integrate_Lum(np.power(10, LogNuArr[:j]), LuminosityFnc, LogRArr[:j]) +
+print('Total Luminosity w/ 1000 steps = ' + str(Lum_Tot))
+
+# Lum_Tot2 = integrate_Lum2(np.power(10, LogNuArr[:j]), LuminosityFnc, LogRArr[:j]) + integrate_Lum2(np.power(10, LogNuArr[j:]), LuminosityFnc, LogRArr[j:])
+# print('Integrate 2 = ' + str(Lum_Tot2))
+
+Lum_Log_Totals = []
+Lum_Totals = []
+steps = [100, 150, 200, 250, 300, 350, 400, 500, 600, 800, 1000, 1200, 1500, 2000]
+for i in steps:
+    LumTotal = integrate_Lum(np.power(10, LogNuArrVar(i)[j:]), LuminosityFnc, LogRArrVar(i)[j:])
+    Lum_Totals.append(LumTotal)
+    Lum_Log_Totals.append(np.log10(LumTotal)) # np.log10
+plt.plot(steps, Lum_Totals)
+plt.xlabel('Steps')
+plt.ylabel('Total luminosity / $W$')
+plt.show()
+plt.plot(steps, Lum_Log_Totals)
+plt.xlabel('Steps')
+plt.ylabel('Log(Total luminosity) / $W$')
 plt.show()
 
-Lum_Tot = integrate_Lum(np.power(10, LogNuArr), LuminosityFnc, LogRArr)
-print('Integrate 1 = ' + str(Lum_Tot))
-Lum_Tot2 = integrate_Lum2(np.power(10, LogNuArr), LuminosityFnc, LogRArr)
-print('Integrate 2 = ' + str(Lum_Tot))
-
 #####################################################################
+
+# 07/11 22:19 Total Luminosity w/ 1000 steps = 8.849751105624847e+30
