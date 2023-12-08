b64d7a757f047dfd6f9e86265363e5222d995a20
TtForge <tobyforge@gmail.com>
2023-12-08 21:21:30 +0000

    Adding Colouring #1

---
+++ Testing/__GraphDesign.py
@@ -0,0 +1,39 @@
+from cProfile import label
+import matplotlib.pyplot as plt
+# from __FunctionBase import *
+from Testing.__FunctionBase import *
+
+plt.rcParams.update({
+    "figure.facecolor":  (1.0, 1.0, 1.0, 0.0),  # red   with alpha = 30%
+    "axes.facecolor":    (1.0, 1.0, 1.0, 0.0),  # green with alpha = 50%
+    "savefig.facecolor": (1.0, 1.0, 1.0, 0.0),  # blue  with alpha = 20%
+    "legend.framealpha": 0.2,
+    "lines.linewidth":   3,
+    "axes.linewidth":    3,
+    "axes.labelsize":    20,
+    "lines.markersize": 10,
+    "xtick.labelsize": 16,
+    "ytick.labelsize": 16,
+    "legend.fontsize": 15,
+    "axes.edgecolor": 'white',
+    "xtick.labelcolor": 'white',
+    "ytick.labelcolor": 'white',
+    "axes.labelcolor": 'white',
+    "legend.labelcolor": 'white',
+    "text.color": 'white',
+    "axes.spines.top":    False,
+    "axes.spines.right":  False,
+    "ytick.color": 'white',
+    "legend.markerscale": 2,
+    "font.family": 'serif',
+    "mathtext.fontset": 'dejavuserif',
+    "savefig.format": 'svg',
+})
+
+colors = ['#fff9dd', '#fee58b', '#ffcb58', '#ffac2b', '#f98811', '#e16904', '#c14f01', '#822a00', '#551400']
+
+
+
+
+
+
